f1e54da09071b6808be96f1a1afafc8fe8b31e73
Amit Yadav <amityadav@amityadav.in>
2017-06-12 16:18:27 +0530

    ForeignKey check in logging

--- emtex_common_utils/mixins.py
+++ emtex_common_utils/mixins.py
@@ -1,3 +1,6 @@
+from django.db import models
+
+
 class BaseModelMixin(object):
     def __init__(self, *args, **kwargs):
         super(BaseModelMixin, self).__init__(*args, **kwargs)
@@ -10,8 +13,14 @@ class BaseModelMixin(object):
         original_value = getattr(self, original_name)
         new_value = getattr(self, field_name)
 
+        # Check for None type
         if original_value in {None, ''} and new_value in {None, ''}:
             return False
+
+        # Check for ForeignKey
+        if isinstance(original_value, models.Model):
+            if original_value.id != new_value.id:
+                return True
 
         if original_value != new_value:
             return True
@@ -21,9 +30,17 @@ class BaseModelMixin(object):
     def get_old_value(self, field_name):
         original_name = '__original_%s' % field_name
         try:
-            return getattr(self, original_name)
+            original_value = getattr(self, original_name)
+            if isinstance(original_value, models.Model):
+                return original_value.id
+            else:
+                return original_value
         except AttributeError:
             raise AttributeError('`%s` is not tracked' % field_name)
 
     def get_new_value(self, field_name):
-        return getattr(self, field_name)
+        new_value = getattr(self, field_name)
+        if isinstance(new_value, models.Model):
+            return new_value.id
+        else:
+            return new_value
